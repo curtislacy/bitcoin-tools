ae88a5cc08936a5c377296289a560a45598436ad
Curtis Lacy <curtis.lacy@grgcomponents.com>
2014-07-10 10:18:13 -0400

    Reworked to check to make sure public and private key addresses at each depth correspond to each other, and that we don't repeat.

--- generate-hd.js
+++ generate-hd.js
@@ -12,24 +12,25 @@ else
 	console.log( 'Base58 public key:  ' + hdWallet.toBase58( false ) );
 	console.log();
 
-/*	console.log( '*** First, the "master node" public and private keys.' );
-	console.log( 'Private key: ' + hdWallet.priv.toWif() );
-	console.log( 'Public Key: ' + hdWallet.pub.toWif() );
-	console.log();*/
+	var prvAddresses = {};
+	var pubAddresses = {};
+	var fromPub = bitcoin.HDNode.fromBase58( hdWallet.toBase58( false ) );
 
-	console.log( '*** Here are your private keys and their corresponding addresses:' );
 	for( var i=1; i<=number; i++ )
 	{
 		var derivedPrivate = hdWallet.derive( i );
-		console.log( 'Key: ' + derivedPrivate.privKey.toWIF() + '   Addr: ' + derivedPrivate.getAddress().toString() );
-	}
-	console.log();
+		prvAddresses[ derivedPrivate.getAddress().toString() ] = true;
+		if( pubAddresses[ derivedPrivate.getAddress().toString() ] )
+			console.log( 'Repeated Address at: ' + i + ': ' + derivedPrivate.getAddress().toString() );
 
-	var fromPub = bitcoin.HDNode.fromBase58( hdWallet.toBase58( false ) );
-	console.log( '*** Now, some derived addresses from the public key only (just to make sure they match):' );
-	for( var i=1; i<=number; i++ )
-	{
 		var derived = fromPub.derive( i );
-		console.log( 'Addr: ' + derived.getAddress().toString() );
+		pubAddresses[ derived.getAddress().toString() ] = true;
+
+		if( derivedPrivate.getAddress().toString() != derived.getAddress().toString() )
+			console.log( 'Addresses mismatch for ' + i + ': ' + derived.getAddress().toString() );
+
+
+		if( i % 10000 == 0 )
+			console.log( i );
 	}
 }
